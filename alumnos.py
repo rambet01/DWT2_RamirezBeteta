ac84ff980919f82ccb35ff0b988f4ab4a37d258a
rambet01 <limberamirezb@gmail.com>
2023-11-24 17:04:47 -0500

    Avance inciso ii

--- alumnos.py
+++ alumnos.py
@@ -5,3 +5,18 @@ class Alumno:
         self.edad = edad
         self.nota = nota
         self.nacionalidad = nacionalidad
+              
+    def leerNota(self):
+        return self.nota
+
+    def registrarNota(self):
+        while True:
+            try:
+                nota = int(input(f"Ingrese nota del siguiente alumno - {self.nombre} {self.apellido}: "))
+                if nota < 0 or nota > 20:
+                    print("La nota debe estar en un rango de 0 a 20. Intente nuevamente.")
+                    continue
+                self.nota = nota
+                break
+            except ValueError as ve:
+                print(f"Ingrese solo números para la nota. {ve}")
