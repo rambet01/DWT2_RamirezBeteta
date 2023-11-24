6b55bb60d4b5958e9c2d30eb9f3e142a94cc7415
rambet01 <limberamirezb@gmail.com>
2023-11-24 17:03:12 -0500

    Avance inciso i

---
+++ alumnos.py
@@ -0,0 +1,7 @@
+class Alumno:
+    def __init__(self, nombre, apellido, edad, nota, nacionalidad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.nota = nota
+        self.nacionalidad = nacionalidad
